5837d115733d594410fb5ba3cab2339266588205
paul-local <nwalipaul353@gmail.com>
2023-10-20 08:46:15 +0100

    navigation

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,42 @@
-import { NavLink,  useNavigate } from 'react-router-dom'
-import front_img from "../assets/ux-live.png"
+import { NavLink, useNavigate } from "react-router-dom";
+import front_img from "../assets/ux-live.png";
 const Navbar = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-danger shadow">
         <div className="container-fluid">
           <ul className="col-12 d-flex justify-content-evenly">
-            <li className="nav-item "><NavLink to='/' className='nav-link text-light'><i className="fa-solid fa-home"></i></NavLink></li>
-            <li className="nav-item "><NavLink to='/' className='nav-link text-light'><i className="fa-solid fa-right-from-bracket"></i></NavLink></li>
-            <li className="nav-item "><NavLink to='/' className='nav-link text-light'><i className="fa-solid fa-paperclip"></i></NavLink></li>
-            <li className="nav-item "><NavLink to='/' className='nav-link text-light'><i className="fa-solid fa-shield"></i></NavLink></li>
-            <li className="nav-item "><NavLink to='/' className='nav-link text-light'><i className="fa-solid fa-gear"></i></NavLink></li>
-            <li className="nav-item "><NavLink to='/' className='nav-link text-light'><img src={front_img} className="img-fluid nav-img" /></NavLink></li>
+            <li className="nav-item ">
+              <NavLink to="/" className="nav-link text-light">
+                <i className="fa-solid fa-home"></i>
+              </NavLink>
+            </li>
+            <li className="nav-item ">
+              <NavLink to="/" className="nav-link text-light">
+                <i className="fa-solid fa-right-from-bracket"></i>
+              </NavLink>
+            </li>
+            <li className="nav-item ">
+              <NavLink to="/" className="nav-link text-light">
+                <i className="fa-solid fa-paperclip"></i>
+              </NavLink>
+            </li>
+            <li className="nav-item ">
+              <NavLink to="/" className="nav-link text-light">
+                <i className="fa-solid fa-shield"></i>
+              </NavLink>
+            </li>
+            <li className="nav-item ">
+              <NavLink to="/" className="nav-link text-light">
+                <i className="fa-solid fa-gear"></i>
+              </NavLink>
+            </li>
+            <li className="nav-item ">
+              <NavLink to="/" className="nav-link text-light">
+                <img src={front_img} className="img-fluid nav-img" />
+              </NavLink>
+            </li>
           </ul>
         </div>
       </nav>
